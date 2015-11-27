1b4c84697652cc72d0a3d8011390c351c5cfd262
Atli <atlie15@ru.is>
2015-11-27 14:22:14 +0000

    Template
    ok

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Not a test\n";
+
+    return 0;
+}
+
---
+++ test/nerd.h
@@ -0,0 +1,11 @@
+#ifndef NERD_H
+#define NERD_H
+
+
+class Nerd
+{
+public:
+    Nerd();
+};
+
+#endif // NERD_H
